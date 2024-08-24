4b3c2a7af868337c26e7d8713efe5abd17c7270d
JoeLaFort <jglafortune@hotmail.com>
2024-08-24 15:15:36 -0400

    Add files via upload

---
+++ Ruffle_Player_Script.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SWF Player</title>
+    <script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
+</head>
+<body>
+    <h1>Embedded SWF File</h1>
+    <div>
+        <embed src=Membrane Transport.swf width="800" height="600">
+    </div>
+</body>
+</html>
